3d5a1c5c5dbaeff35d9131e58fde612dd96150a5
Jawabreh0 <jawabreh-social@protonmail.com>
2024-11-01 14:03:06 +0200

    openai and aws bedrock config

---
+++ src/config/config.ts
@@ -0,0 +1,6 @@
+export const config = {
+  openaiApiKey: process.env.OPENAI_API_KEY || "",
+  bedrockRegion: process.env.AWS_REGION || "eu-west-2",
+  awsAccessKey: process.env.AWS_ACCESS_KEY_ID || "",
+  awsSecretKey: process.env.AWS_SECRET_ACCESS_KEY || "",
+};
