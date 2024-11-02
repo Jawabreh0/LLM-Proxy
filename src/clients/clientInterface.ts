cf2f46ad4dd7c6d1395c779e4b53d122beac202d
Jawabreh0 <jawabreh-social@protonmail.com>
2024-11-02 23:34:33 +0200

    accept stream param, update generateText to generateCompletion

--- src/clients/clientInterface.ts
+++ src/clients/clientInterface.ts
@@ -6,6 +6,7 @@ export interface ClientInterface {
     messages: OpenAIMessages, // TODO: when we add more providers we need to update this
     model?: OpenAISupportedLLMs, // TODO: when we add more providers we need to update this
     maxTokens?: number,
-    temperature?: number
+    temperature?: number,
+    stream?: boolean
   ): Promise<string>;
 }
