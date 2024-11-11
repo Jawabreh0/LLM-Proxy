e69b22cfc1d9191e0dad1d405766817ef9576d3e
Jawabreh0 <jawabreh-social@protonmail.com>
2024-11-11 02:26:40 +0200

    feat: examples

---
+++ examples/src/testNonStreamOpenAI.ts
@@ -0,0 +1,44 @@
+import {
+  generateLLMResponse,
+  OpenAIMessages,
+  OpenAIMessagesRoles,
+  OpenAISupportedLLMs,
+} from "llm-proxy/dist";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+async function testNonStreamOpenAI() {
+  try {
+    const messages: OpenAIMessages = [
+      {
+        role: OpenAIMessagesRoles.USER,
+        content: "Say hello!",
+      },
+    ];
+
+    const response = await generateLLMResponse(
+      messages,
+      {
+        type: "OpenAI",
+        model: OpenAISupportedLLMs.GPT_4_O,
+      },
+      1000,
+      0.7,
+      "You are a helpful assistant",
+      [],
+      {
+        apiKey: process.env.OPENAI_API_KEY,
+      }
+    );
+
+    console.log("Assistant Response:", response.choices[0].message.content);
+    console.log("Usage:");
+    console.log("Input", response.usage.prompt_tokens);
+    console.log("Output Tokens", response.usage.completion_tokens);
+  } catch (error) {
+    console.error("OpenAI Error:", error);
+  }
+}
+
+export { testNonStreamOpenAI };
